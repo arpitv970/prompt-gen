2187f3fca98c5a676ffe16b6a06f2e3495d69474
Arpit Verma <arpitv970@gmail.com>
2023-07-09 19:47:03 +0530

    implemented copy to clipboard

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,5 +1,55 @@
+'use client';
+
+import { useState, useEffect } from 'react';
+import PromptCard from './PromptCard';
+
+const PromptCardList = ({ data, handleTagClick }) => {
+    return (
+        <div className='mt-10 prompt_layout'>
+            {data &&
+                data.map((post) => (
+                    <PromptCard
+                        key={post._id}
+                        post={post}
+                        handleTagClick={handleTagClick}
+                    />
+                ))}
+        </div>
+    );
+};
+
 const Feed = () => {
-    return <div>Feed</div>;
+    const [searchText, setSearchText] = useState('');
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        const fetchPost = async () => {
+            const res = await fetch('/api/prompt');
+            const data = await res.json();
+
+            setPosts(data);
+        };
+
+        fetchPost();
+    }, []);
+
+    const handleSearchChange = (e) => {};
+    return (
+        <section className='feed'>
+            <form className='relative w-full flex-center'>
+                <input
+                    type='text'
+                    placeholder='Search for a tag or a username'
+                    value={searchText}
+                    onChange={handleSearchChange}
+                    required
+                    className='search_input peer'
+                />
+            </form>
+
+            <PromptCardList data={posts} handleTagClick={() => {}} />
+        </section>
+    );
 };
 
 export default Feed;
