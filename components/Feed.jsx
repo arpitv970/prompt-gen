bf3648beffeb6559fa9b97ab073ff67d93cffd05
Arpit Verma <arpitv970@gmail.com>
2023-07-23 01:04:22 +0530

    implemented delete posts functionality

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -6,13 +6,14 @@ import PromptCard from './PromptCard';
 const PromptCardList = ({ data, handleTagClick }) => {
     return (
         <div className='mt-10 prompt_layout'>
-            {data && data.map((post) => (
-                <PromptCard
-                    key={post._id}
-                    post={post}
-                    handleTagClick={handleTagClick}
-                />
-            ))}
+            {data &&
+                data.map((post) => (
+                    <PromptCard
+                        key={post._id}
+                        post={post}
+                        handleTagClick={handleTagClick}
+                    />
+                ))}
         </div>
     );
 };
@@ -32,15 +33,21 @@ const Feed = () => {
         fetchPost();
     }, []);
 
-    const handleSearchChange = (e) => {};
+    const handleSearchChange = (e) => {
+        e.preventDefault();
+        setSearchText(e.target.value);
+        console.log(e.target.value);
+    };
     return (
         <section className='feed'>
-            <form className='relative w-full flex-center'>
+            <form
+                onChange={handleSearchChange}
+                className='relative w-full flex-center'
+            >
                 <input
                     type='text'
                     placeholder='Search for a tag or a username'
                     value={searchText}
-                    onChange={handleSearchChange}
                     required
                     className='search_input peer'
                 />
--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -58,7 +58,7 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
                     handleTagClick && handleTagClick(post.tag);
                 }}
             >
-                {post.tag}
+                #{post.tag}
             </p>
             {session?.user.id === post.creator._id &&
                 pathName === '/profile' && (
